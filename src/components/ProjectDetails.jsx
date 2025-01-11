d50f42025c7c61b264e7a5ec54e9e2565bbbeb4e
rimaakter1 <rimac201262@gmail.com>
2025-01-12 03:24:20 +0600

    fixed bugs

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -1,21 +1,33 @@
-import React from "react";
 import { useParams } from "react-router-dom";
+import project1 from "../../src/assets/project-1.jpg";
+import project2 from "../../src/assets/project-2.avif";
+import project3 from "../../src/assets/project-3.jpg";
 
 const projectDetails = {
     1: {
         id: 1,
-        name: "Volunteer Management Website(BhaloKaj)",
+        name: "Volunteer Management Website (BhaloKaj)",
         client: {
             stack: "React, Tailwind CSS, daisyUI",
             github: "https://github.com/programming-hero-web-course2/b10a11-client-side-Rimaakter1?tab=readme-ov-file",
             live: "https://bhalokaj-a037f.web.app/",
         },
         server: {
-            stack: " MongoDB, Express JS",
+            stack: "MongoDB, Express JS",
             github: "https://github.com/programming-hero-web-course2/b10a11-server-side-Rimaakter1",
         },
-        description: "This is the  implementation of the Volunteer Management Website. It provides a user interface for creating, updating, and managing volunteer opportunities and allows users to sign up as volunteers for posted opportunities.",
-        image: "/src/assets/project-1.jpg",
+        features: [
+            "Login and Registration with email/password",
+            "Google / GitHub social login",
+            "Fully responsive for mobile, tablet, and desktop devices.",
+            "Dark/Light mode toggle",
+            "Interactive layout switching for volunteer need posts",
+            "Add, update, and delete volunteer opportunities",
+            "View posts sorted by upcoming deadlines",
+            "Search functionality for post titles"
+        ],
+        description: "This website offers a user-friendly platform for managing volunteer opportunities, allowing administrators to create, update, and organize events while enabling users to easily sign up as volunteers for listed opportunities.",
+        image: project1,
     },
     2: {
         id: 2,
@@ -26,12 +38,19 @@ const projectDetails = {
             live: "https://gameecho-ca02a.web.app/",
         },
         server: {
-            stack: " MongoDB, Express JS",
+            stack: "MongoDB, Express JS",
             github: "#",
         },
+        features: [
+            "Explore and share detailed game reviews with user-friendly navigation and interactive interfaces",
+            "Accessible on mobile, tablet, and desktop devices",
+            "Secure email/password login and social authentication (Google or GitHub)",
+            "Can be sort and filter reviews by genre, year, or rating",
+            "Add reviews, manage a personalized watchlist, and more, all with real-time database"
+        ],
         description:
-            "GameEcho is a user-friendly platform designed to enhance the gaming community by providing a space to explore, review, and share opinions on games. With its clean and responsive interface, it ensures a Continuous experience across devices, making it easy for users to manage reviews and build a personalized watchlist.",
-        image: "/src/assets/project-2.avif",
+            "GameEcho is a user-friendly platform designed to enhance the gaming community by providing a space to explore, review, and share opinions on games. With its clean and responsive interface, it ensures a continuous experience across devices, making it easy for users to manage reviews and build a personalized watchlist.",
+        image: project2,
     },
     3: {
         id: 3,
@@ -39,9 +58,16 @@ const projectDetails = {
         stack: "React, Firebase, Tailwind CSS, DaisyUI",
         github: "https://github.com/programming-hero-web-course1/b10-a9-authentication-Rimaakter1",
         live: "https://discountden.web.app/",
+        features: [
+            "Email and password-based login and registration",
+            "Google social login using Firebase Authentication",
+            "View discount coupons for various brands with details like discount amount, expiry date, and conditions",
+            "Copy coupon codes using react-copy-to-clipboard",
+            "Fully responsive layout optimized for mobile, tablet, and desktop devices."
+        ],
         description:
             "Discount PRO is a user-friendly application designed to help users find and use discount coupons for popular e-commerce shops. It simplifies the process of discovering and applying discount codes, making it easier for users to save money.",
-        image: "/src/assets/project-3.avif",
+        image: project3,
     }
 };
 
@@ -63,7 +89,7 @@ const ProjectDetails = () => {
                     <img
                         src={project.image}
                         alt={project.name}
-                        className="w-full rounded-md shadow-lg mb-8"
+                        className="w-full h-96 object-cover rounded-md shadow-lg mb-8"
                     />
                     <h3 className="text-lg text-teal-400 font-semibold mb-4">
                         Full Description:
@@ -93,13 +119,12 @@ const ProjectDetails = () => {
                                 GitHub
                             </a>
                         </div>
-                        {
-                            project?.server && <div>
+                        {project?.server && (
+                            <div>
                                 <h4 className="text-xl text-gray-200 font-semibold mb-2">
                                     Server Side
                                 </h4>
                                 <p className="text-gray-400 mb-4">{project?.server?.stack}</p>
-                                <p className="text-gray-400 mb-4">{project?.server?.details}</p>
                                 <a
                                     href={project?.server?.github}
                                     className="btn btn-secondary mb-2"
@@ -109,8 +134,19 @@ const ProjectDetails = () => {
                                     GitHub
                                 </a>
                             </div>
-                        }
+                        )}
                     </div>
+                    <div className="mt-8">
+                        <h3 className="text-lg text-teal-400 font-semibold mb-4">
+                            Project Features:
+                        </h3>
+                        <ul className="list-disc list-inside text-gray-300 space-y-2">
+                            {project.features.map((feature, index) => (
+                                <li key={index}>{feature}</li>
+                            ))}
+                        </ul>
+                    </div>
+
                 </div>
             </div>
         </section>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
+import project1 from "../../src/assets/project-1.jpg";
+import project2 from "../../src/assets/project-2.avif";
+import project3 from "../../src/assets/project-3.jpg";
 
 const projects = [
     {
@@ -12,11 +15,11 @@ const projects = [
             live: "https://bhalokaj-a037f.web.app/",
         },
         server: {
-            stack: " MongoDB, Express JS",
+            stack: "MongoDB, Express JS",
             github: "https://github.com/programming-hero-web-course2/b10a11-server-side-Rimaakter1",
         },
-        description: "This is the  implementation of the Volunteer Management Website. It provides a user interface for creating, updating, and managing volunteer opportunities and allows users to sign up as volunteers for posted opportunities.",
-        image: "/src/assets/project-1.jpg",
+        description: "This is the implementation of the Volunteer Management Website. It provides a user interface for creating, updating, and managing volunteer opportunities and allows users to sign up as volunteers for posted opportunities.",
+        image: project1,
     },
     {
         id: 2,
@@ -27,12 +30,12 @@ const projects = [
             live: "https://gameecho-ca02a.web.app/",
         },
         server: {
-            stack: " MongoDB, Express JS",
+            stack: "MongoDB, Express JS",
             github: "#",
         },
         description:
-            "GameEcho is a user-friendly platform designed to enhance the gaming community by providing a space to explore, review, and share opinions on games. With its clean and responsive interface, it ensures a Continuous experience across devices, making it easy for users to manage reviews and build a personalized watchlist.",
-        image: "/src/assets/project-2.avif",
+            "GameEcho is a user-friendly platform designed to enhance the gaming community by providing a space to explore, review, and share opinions on games. With its clean and responsive interface, it ensures a continuous experience across devices, making it easy for users to manage reviews and build a personalized watchlist.",
+        image: project2,
     },
     {
         id: 3,
@@ -42,10 +45,9 @@ const projects = [
         live: "https://discountden.web.app/",
         description:
             "Discount PRO is a user-friendly application designed to help users find and use discount coupons for popular e-commerce shops. It simplifies the process of discovering and applying discount codes, making it easier for users to save money.",
-        image: "/src/assets/project-3.avif",
+        image: project3,
     },
 ];
-
 
 const Projects = () => {
     return (
@@ -54,11 +56,11 @@ const Projects = () => {
                 <h2 className="text-5xl font-extrabold text-center text-teal-400 mb-12">
                     My Projects
                 </h2>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-11/12 mx-auto gap-8">
                     {projects.map((project) => (
                         <motion.div
                             key={project.id}
-                            className="group rounded-xl overflow-hidden shadow-lg bg-gray-800 hover:bg-gradient-to-br hover:from-teal-900 hover:to-gray-700 transform transition duration-300 hover:scale-105"
+                            className="group rounded-xl overflow-hidden shadow-lg bg-gray-800 hover:bg-gradient-to-br hover:from-teal-900 hover:to-gray-700 transform transition duration-300 hover:scale-105 flex flex-col justify-between h-full"
                             whileHover={{ scale: 1.05 }}
                         >
                             <motion.figure
@@ -71,18 +73,16 @@ const Projects = () => {
                                     className="w-full h-48 object-cover transform group-hover:scale-105 transition-all duration-300"
                                 />
                             </motion.figure>
-                            <div className="p-6">
-                                <h3 className="text-2xl font-bold text-teal-300 mb-2 group-hover:text-teal-400 transition-colors duration-300">
-                                    {project.name}
-                                </h3>
-                                <p className="text-gray-400 text-sm mb-4">
-                                    {project.description}
-                                </p>
-                                <div className="mt-4">
+                            <div className="p-6 flex flex-col justify-between flex-grow">
+                                <div>
+                                    <h3 className="text-2xl font-bold text-teal-300 mb-2 group-hover:text-teal-400 transition-colors duration-300">
+                                        {project.name}
+                                    </h3>
+                                    <p className="text-gray-400 text-sm mb-4 mt-3">
+                                        {project.description.length > 100 ? `${project.description.slice(0, 100)}...` : project.description}
+                                    </p>
                                     <h4 className="text-lg font-semibold text-gray-300 mb-2">
-                                        {
-                                            project?.client?.stack ? "Client Side" : "Links"
-                                        }
+                                        {project?.client?.stack ? "Client Side" : "Links"}
                                     </h4>
                                     <p className="text-gray-400 text-sm mb-2">{project?.client?.stack || project.stack}</p>
                                     <div className="flex gap-3 mb-4">
@@ -105,33 +105,34 @@ const Projects = () => {
                                             <span>GitHub</span>
                                         </a>
                                     </div>
-                                </div>
-                                {
-                                    project?.server?.stack && <div>
-                                        <h4 className="text-lg font-semibold text-gray-300 mb-2">
-                                            Server Side
-                                        </h4>
-                                        <p className="text-gray-400 text-sm mb-2">{project?.server?.stack}</p>
-                                        <div className="flex gap-3">
-                                            <a
-                                                href={project?.server?.github}
-                                                target="_blank"
-                                                rel="noopener noreferrer"
-                                                className="btn btn-secondary flex items-center space-x-2 px-4 py-2 hover:bg-gray-600"
-                                            >
-                                                <FaGithub />
-                                                <span>GitHub</span>
-                                            </a>
-
+                                    {project?.server?.stack && (
+                                        <div>
+                                            <h4 className="text-lg font-semibold text-gray-300 mb-2">
+                                                Server Side
+                                            </h4>
+                                            <p className="text-gray-400 text-sm mb-2">{project?.server?.stack}</p>
+                                            <div className="flex gap-3">
+                                                <a
+                                                    href={project?.server?.github}
+                                                    target="_blank"
+                                                    rel="noopener noreferrer"
+                                                    className="btn btn-secondary flex items-center space-x-2 px-4 py-2 hover:bg-gray-600"
+                                                >
+                                                    <FaGithub />
+                                                    <span>GitHub</span>
+                                                </a>
+                                            </div>
                                         </div>
-                                    </div>
-                                }
-                                <Link
-                                    to={`/project/${project.id}`}
-                                    className="btn btn-accent flex items-center space-x-2 px-4 py-2 hover:bg-teal-600 mt-8"
-                                >
-                                    <span>Details</span>
-                                </Link>
+                                    )}
+                                </div>
+                                <div className="mt-auto">
+                                    <Link
+                                        to={`/project/${project.id}`}
+                                        className="btn btn-accent flex items-center space-x-2 px-4 py-2 hover:bg-teal-600 mt-8"
+                                    >
+                                        <span>Details</span>
+                                    </Link>
+                                </div>
                             </div>
                         </motion.div>
                     ))}
